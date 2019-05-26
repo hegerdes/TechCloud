9a6bb0287e0278f65217a5ad51b916b800b2ebdb
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2019-05-26 23:50:34 +0200

    INIT

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "src\ui\MainWindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow window;
+
+    //window.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
+    window.show();
+
+    return a.exec();
+}
