3298cd2492221363ad8cc9e379f34eaa5de0f682
Henrik Gerdes <hegerdes@uni-osnabrueck.de>
2019-06-03 01:07:54 +0200

    redesing result window

--- src/ui/ResultWindow.cpp
+++ src/ui/ResultWindow.cpp
@@ -20,20 +20,12 @@ ResultWindow::ResultWindow(QWidget *parent) :
     ui(new Ui::ResultWindow)
 {
     ui->setupUi(this);
-    ui->toolBox->close();
-    ui->frame->close();
 
 }
 
 ResultWindow::~ResultWindow()
 {
     delete ui;
-}
-
-void ResultWindow::on_pushButton_clicked()
-{
-    ui->toolBox->show();
-    ui->frame->show();
 }
 
 } //tcloud
